8b918ded5f9c47e76ed18476315e6968562d42c0
Brandon Hawkins <brandonhawkins8@gmail.com>
2019-03-20 00:22:15 -0400

    Started to work on input, but it is currently non-functional.
    Look for the FIXME comment in the `_process(delta)` function of
    `Stage.gd`.

--- characters/base/Character.gd
+++ characters/base/Character.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+# Signals whether the player wins the round.
+signal win_round
+
+# Signals whether the player wins the game (by winning two rounds).
+signal win_game
+
 # Hitpoints of the character. Once this reaches 0, the player
 # is defeated and the opponent wins
 export (int) var hitpoints = 200
@@ -9,7 +15,7 @@ export (PackedScene) var Character
 
 # Signifies which player an instanced Character is.
 # 0 is player 1, 1 is player 2.
-var player = 0
+var player_number = 0
 
 # Standard tempo the animations are timed to in beats per minute.
 const default_bpm = 120.0
@@ -19,14 +25,20 @@ const default_fps = 60.0
 
 # The multiplier for adjusting the speed of the animation by BPM.
 # See the function "set_speed_of_animation_by_BPM".
-const animation_factor = default_fps / default_bpm
+const default_animation_factor = default_fps / default_bpm
+
+# Used for movement on the one-dimensional grid in a stage.
+var grid_number = 0
+
+# Round wins
+onready var wins = 0
 
 func _ready():
 	pass
 
 # Sets the speed of the animations by bpm.
 func set_speed_of_animation_by_BPM(bpm):
-	var new_fps = bpm * animation_factor
+	var new_fps = bpm * default_animation_factor
 	$AnimatedSprite.frames.set_animation_speed("idle", new_fps)
 	pass
 
--- stages/base/Stage.gd
+++ stages/base/Stage.gd
@@ -9,40 +9,94 @@ export (int) var bpm = 120.0
 # Note: Do not assign the position markers to other variables. It will throw
 # errors in the debugger. -Brandon Hawkins
 
-var is_game_over = false
+onready var is_game_over = false
+
+onready var is_input_allowed = false
+
+onready var tween = get_node("Tween")
 
 func _ready():
+	# Sets up Player1
 	$Player1.set_speed_of_animation_by_BPM(bpm)
 	$Player1.hide()
 	$Player1.position = $Position0.position
+	$Player1.grid_number = 0
 	$Player1/AnimatedSprite.animation = "idle"
 	$Player1/AnimatedSprite.playing = false
 	$Player1/AnimatedSprite.frame = 0
 	$Player1/AnimatedSprite.flip_h = false
-	$Player1.player = 0
+	$Player1.player_number = 0
 	
-	$Player2.hide()
+	# Sets up Player2
 	$Player2.set_speed_of_animation_by_BPM(bpm)
+	$Player2.hide()
 	$Player2.position = $Position8.position
+	$Player2.grid_number = 8
 	$Player2/AnimatedSprite.animation = "idle"
 	$Player2/AnimatedSprite.playing = false
 	$Player2/AnimatedSprite.frame = 0
 	$Player2/AnimatedSprite.flip_h = true
-	$Player2.player = 1
+	$Player2.player_number = 1
 	
+	# Starts the game 
 	$StartTimer.start()
 	pass
 
 func _process(delta):
-	if !is_game_over:
-		if $Player1.position.x >= $Player2.position.x and $Player2.position.x <= $Player1.position.x:
-			$Player1/AnimatedSprite.flip_h = true
-			$Player2/AnimatedSprite.flip_h = false
-		else:
-			$Player1/AnimatedSprite.flip_h = false
-			$Player2/AnimatedSprite.flip_h = true
+	if !is_game_over and is_input_allowed:
+		if $Player1.grid_number != $Player2.grid_number:
+			if $Player1.position.x > $Player2.position.x and $Player2.position.x < $Player1.position.x:
+				$Player1/AnimatedSprite.flip_h = true
+				$Player2/AnimatedSprite.flip_h = false
+			else:
+				$Player1/AnimatedSprite.flip_h = false
+				$Player2/AnimatedSprite.flip_h = true
+			
+			# FIXME: Input is not working at all.
+			if Input.is_action_pressed("p1_left"):
+				if $Player1.grid_number != 0 and $Player1.grid_number != $Player2.grid_number + 1:
+					set_position($Player1, $Player1.grid_number - 1)
+			if Input.is_action_pressed("p1_right"):
+				if $Player1.grid_number != 8 and $Player1.grid_number != $Player2.grid_number - 1:
+					set_position($Player1, $Player1.grid_number + 1)
+			if Input.is_action_pressed("p2_left"):
+				if $Player2.grid_number != 0 and $Player2.grid_number != $Player2.grid_number + 1:
+					set_position($Player2, $Player2.grid_number - 1)
+			if Input.is_action_pressed("p2_right"):
+				if $Player1.grid_number != 8 and $Player2.grid_number != $Player1.grid_number - 1:
+					set_position($Player2, $Player2.grid_number + 1)
 	pass
 
+# Sets the position of the player characters.
+func set_position(main_player, grid_number):
+	if grid_number == 0:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position0.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 1:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position1.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 2:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position2.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 3:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position3.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 4:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position4.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 5:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position5.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 6:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position6.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 7:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position7.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	if grid_number == 8:
+		tween.interpolate_property(main_player, "position", main_player.position,
+		$Position8.position, .2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	pass
 
 func _on_StartTimer_timeout():
 	$Player1.show()
@@ -52,8 +106,12 @@ func _on_StartTimer_timeout():
 	$Player2/AnimatedSprite.playing = true
 	
 	$GameTimer.start()
+	
+	is_input_allowed = true
+	
 	pass
 
 func _on_GameTimer_timeout():
 	is_game_over = true
-	pass # replace with function body
+	is_input_allowed = false
+	pass
